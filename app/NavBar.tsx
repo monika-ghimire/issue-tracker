26d7c142290d5693f76ecacb3a732fb23f2b9ba3
Monika ghimire <monika.ghimirey@boosoft.com.au>
2025-10-26 12:34:32 +0545

    style active classnames in navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,14 +1,20 @@
+'use client';
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 import {FaBug} from 'react-icons/fa'
+import classNames from 'classnames';
 
 const NavBar = () => {
+  const currentPath = usePathname();
+       
     const links = [
         {label : 'Dashboard', href:'/'},
         {label : 'Issues', href:'/issues'},
-
     ]
+
   return (
     <nav className="flex items-center gap-6 border-b mb-5 px-5 h-14 ">
       <Link href="/">
@@ -19,7 +25,11 @@ const NavBar = () => {
         {links.map(link => 
         <Link 
         key={link.href} 
-        className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+        className={classNames({
+            'text-zinc-900 font-semibold' : link.href === currentPath,
+            'text-zinc-500' : link.href !== currentPath,
+            'hover:text-zinc-800 transition-colors' : true,
+        })}
         href={link.href}>
         {link.label}
         </Link>
